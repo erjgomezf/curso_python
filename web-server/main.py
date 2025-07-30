84460adce68d5cbb4db6d6c480ee4a7c1e73e385
Ernesto Gomez <requien330@gmail.com>
2025-07-30 14:40:52 -0400

    Agregar implementación básica de FastAPI con rutas para obtener listas y HTML

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,32 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+
+@app.get('/pares')
+def get_list():
+    return {'name': 'Juan', 'age': 30}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>Hay texto HTML aca</title>
+        </head>
+        <body>
+            <h1>Soy un parrafo! HTML!</h1>
+        </body>
+    </html>
+    """
+
 
 def run():
     store.get_categories()
