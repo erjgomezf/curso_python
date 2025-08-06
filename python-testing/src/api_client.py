38edec4e361c0ea5f2ce40f03040627ba8fc5564
Ernesto Gomez <requien330@gmail.com>
2025-08-06 16:59:08 -0400

    pruebas en test

--- python-testing/src/api_client.py
+++ python-testing/src/api_client.py
@@ -1,16 +1,24 @@
 import requests
+import ipaddress
 
 def get_location(ip):
+    try:
+        ipaddress.ip_address(ip)
+    except ValueError:
+        raise ValueError("Invalid IP address")
+        
     url = f"https://freeipapi.com/api/json/{ip}"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
-    import ipdb; ipdb.set_trace()
+    #import ipdb; ipdb.set_trace() # Se debe eliminar esta línea en producción
     return {
         "country": data["countryName"],
         "region": data["regionName"],
         "city": data["cityName"],
     }
 
+
 if __name__ == "__main__":
     print(get_location("8.8.8.8"))
+    
--- python-testing/test/test_api_client.py
+++ python-testing/test/test_api_client.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, requests
 from src.api_client import get_location
 from unittest.mock  import patch
 
@@ -18,4 +18,40 @@ class ApiClientTests(unittest.TestCase):
         self.assertEqual(result.get("region"), "FLORIDA")
         self.assertEqual(result.get("city"), "MIAMI")
 
-        mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8")
+        mock_get.assert_called_once_with("https://freeipapi.com/api/json/8.8.8.8") #Confirma que se llama a la URL correcta
+        
+    @patch('src.api_client.requests.get')
+    def test_get_location_returns_side_effect(self, mock_get):
+        mock_get.side_effect = [
+            requests.exceptions.RequestException("Error de conexión"),
+            unittest.mock.Mock(status_code=200, json=lambda: {
+                "countryName": "USA",
+                "regionName": "FLORIDA",
+                "cityName": "MIAMI",
+            })
+        ]
+
+        with self.assertRaises(requests.exceptions.RequestException):
+            get_location("8.8.8.8")  # Lanza una excepción de conexión
+                         
+        result = get_location("8.8.8.8") # Llama a la segunda respuesta simulada
+        self.assertEqual(result.get("country"), "USA")
+        self.assertEqual(result.get("region"), "FLORIDA")
+        self.assertEqual(result.get("city"), "MIAMI")
+        
+        """RETO
+        Si envia una validación para una ip inválida,
+        debería lanzar una excepción de tipo ValueError.
+        
+        si envia una ip valida, debería retornar un diccionario con las claves country, region y city.
+        """
+
+    def test_get_location_with_invalid_ip_raises_value_error(self):
+        """Verifica que se lanza ValueError con una IP inválida."""
+        with self.assertRaises(ValueError):
+            get_location("invalid_ip")
+
+    def test_get_location_with_empty_ip_raises_value_error(self):
+        """Verifica que se lanza ValueError con una IP vacía."""
+        with self.assertRaises(ValueError):
+            get_location("")
