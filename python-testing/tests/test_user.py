8be4e23ffdd1b41ce3d137f23c96e270e8768105
Ernesto Gomez <requien330@gmail.com>
2025-08-13 09:51:08 -0400

    pruebas con Faker

--- python-testing/tests/test_user.py
+++ python-testing/tests/test_user.py
@@ -10,6 +10,8 @@ class UserTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.faker = Faker(locale="es")
+        self.user = User(self.faker.name(), self.faker.email())
+        
         
     def test_user_creation(self):
         name_generated = self.faker.name()
@@ -19,3 +21,21 @@ class UserTest(unittest.TestCase):
         self.assertEqual(user.name, name_generated)
         self.assertEqual(user.email, email_generated)
         
+    def test_user_with_multiple_account(self):
+        
+        
+        for _ in range(3):
+            bank_account = BankAccount(
+                self.faker.random_int(min=1000, max=9999, step = 50),
+                log_file = self.faker.file_name(extension='.txt')
+            )
+            
+            self.user.add_account(account = bank_account)
+            
+        expected_value = self.user.get_total_balance()
+        value = sum(account.get_balance() for account in self.user.accounts)
+        self.assertEqual(expected_value, value, "El saldo total del usuario debería ser igual a la suma de los saldos de sus cuentas")
+        
+    def tearDown(self) -> None:
+        for account in self.user.accounts:
+            os.remove(account.log_file)
